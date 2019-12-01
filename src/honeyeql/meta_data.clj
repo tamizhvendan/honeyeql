0e32b81e8baacbe33be0b9a87e428f92a189a5ae
Tamizhvendan S <tamizh88@gmail.com>
2019-12-01 22:32:03 +0530

    add get columns meta-data [WIP [ch21]

--- src/honeyeql/meta_data.clj
+++ src/honeyeql/meta_data.clj
@@ -12,46 +12,73 @@
   (rs/datafiable-result-set result-set db-spec {:builder-fn rs/as-unqualified-lower-maps}))
 
 (defn- db-ident
+  ([relation]
+   (keyword (inf/hyphenate relation)))
   ([schema relation]
-    (keyword (inf/hyphenate schema) (inf/hyphenate relation))))
+   (keyword (inf/hyphenate schema) (inf/hyphenate relation))))
 
-#_ (db-ident "public" "user")
-#_ (db-ident "public" "invoice_tax")
-#_ (db-ident "meta_data" "invoice_tax")
+#_(db-ident "public" "user")
+#_(db-ident "public" "invoice_tax")
+#_(db-ident "meta_data" "invoice_tax")
 
-(defn- to-entity-meta-data [{:keys [remarks table_type table_schem table_name]}]
-  [(db-ident table_schem table_name)
-   {:db/doc         remarks
-    :db/ident       (db-ident table_schem table_name)
-    :db.entity/type (case table_type
-                      "TABLE" :table
-                      "VIEW" :view)
-    :db/schema      table_schem
-    :db/relation    table_name}])
+(defn- to-entity-meta-data [{:keys [schema]}
+                            {:keys [remarks table_type table_schem table_name]}]
+  (let [ident (if (= (:default schema) table_schem)
+                (db-ident table_name)
+                (db-ident table_schem table_name))]
+    [ident
+     {:db/doc         remarks
+      :db/ident       ident
+      :db.entity/type (case table_type
+                        "TABLE" :table
+                        "VIEW" :view)
+      :db/schema      table_schem
+      :db/relation    table_name}]))
 
-#_ (inf/hyphenate "meta_data")
+#_(inf/hyphenate "meta_data")
 
-(defn- entities-meta-data [db-spec jdbc-meta-data]
+(defn- entities-meta-data [db-spec jdbc-meta-data db-config]
   (->> (into-array String ["TABLE" "VIEW"])
        (.getTables jdbc-meta-data nil "%" nil)
        (meta-data-result db-spec)
-       (map to-entity-meta-data)
+       (map #(to-entity-meta-data db-config %))
        (into {})))
 
-(defn fetch [db-spec]
+
+(defn- filter-columns [db-config columns]
+  (remove #(contains? (get-in db-config [:schema :ignore]) (:table_schem %)) columns))
+
+#_ (filter-columns {:schema {:ignore #{"information_schema"}}} 
+                   [{:table_schem "information_schema"}
+                    {:table_schem "public"}]) 
+
+(defn- add-attributes-meta-data [db-spec jdbc-meta-data db-config heql-meta-data]
+  (->> (.getColumns jdbc-meta-data nil "%" "%" nil)
+       (meta-data-result db-spec)
+       (filter-columns db-config)
+       (assoc heql-meta-data :attributes)))
+
+(defn fetch [db-spec db-config]
   (with-open [conn (jdbc/get-connection db-spec)]
-    (let [jdbc-meta-data (.getMetaData conn)]
-      (entities-meta-data db-spec jdbc-meta-data))))
+    (let [jdbc-meta-data     (.getMetaData conn)
+          entities-meta-data (entities-meta-data db-spec jdbc-meta-data db-config)
+          heql-meta-data     {:entities entities-meta-data}]
+      (spit "./dev/columns.edn" (vec (:attributes (add-attributes-meta-data db-spec jdbc-meta-data heql-meta-data db-config)))))))
 
-#_(fetch db-spec)
+#_(fetch db-spec {:schema {:default "public"
+                           :ignore  #{"information_schema" "pg_catalog"}}})
 
-#_{:public/user         {:db/doc         nil
-                         :db/ident       :public/user
-                         :db.entity/type :table
-                         :db/schema      "public"
-                         :db/relation    "user"}
-   :public/invoice-item {:db/doc         nil
-                         :db/ident       :public/invoice-item
-                         :db.entity/type :table
-                         :db/schema      "public"
-                         :db/relation    "invoice_item"}}
+#_{:user         {:db/doc         nil
+                  :db/ident       :public/user
+                  :db.entity/type :table
+                  :db/schema      "public"
+                  :db/relation    "user"}
+   :invoice-item {:db/doc         nil
+                  :db/ident       :public/invoice-item
+                  :db.entity/type :table
+                  :db/schema      "public"
+                  :db/relation    "invoice_item"}}
+
+(comment
+  (def columns (read-string (slurp "./dev/columns.edn")))
+  (distinct (keys (group-by :table_schem columns))))
