a8692493932889ebde7c6051ae2ab356cbe4bf0d
Tamizhvendan S <tamizh88@gmail.com>
2019-12-05 09:57:58 +0530

    add primary key in entity metadata [ch21]

--- src/honeyeql/meta_data.clj
+++ src/honeyeql/meta_data.clj
@@ -19,7 +19,7 @@
 (defn- attribute-ident [db-config {:keys [table_schem table_name column_name]}]
   (if (= (get-in db-config [:schema :default]) table_schem)
     (keyword (inf/singular (inf/hyphenate table_name)) (inf/hyphenate column_name))
-    (keyword (str (inf/hyphenate table_schem) "." (inf/singular (inf/hyphenate table_name))) 
+    (keyword (str (inf/hyphenate table_schem) "." (inf/singular (inf/hyphenate table_name)))
              (inf/hyphenate column_name))))
 
 (defn- column-ident [db-config {:keys [table_schem table_name column_name]}]
@@ -50,13 +50,13 @@
 (defn- filter-columns [db-config columns]
   (remove #(contains? (get-in db-config [:schema :ignore]) (:table_schem %)) columns))
 
-(defmulti derive-column-type
+(defmulti derive-attr-type
   (fn [db-config _]
     (:db-product-name db-config)))
 
 (defn- add-attribute-meta-data [db-config heql-meta-data
                                 {:keys [table_schem table_name column_name data_type column_size
-                                        remarks is_nullable is_autoincrement type_name]
+                                        remarks is_nullable is_autoincrement type_name ordinal_position]
                                  :as   column-meta-data}]
   (let [attr-ident            (attribute-ident db-config column-meta-data)
         entity-ident          (entity-ident db-config column-meta-data)
@@ -68,7 +68,7 @@
      (assoc-in heql-meta-data [:attributes attr-ident]
                {:attr/ident                     attr-ident
                 :attr/doc                       remarks
-                :attr/type                      (derive-column-type db-config column-meta-data)
+                :attr/type                      (derive-attr-type db-config column-meta-data)
                 :attr/nullable                  is-nullable
                 :attr.column/name               column_name
                 :attr.column/schema             table_schem
@@ -77,7 +77,8 @@
                 :attr.column/auto-incrementable (coarce-boolean is_autoincrement)
                 :attr.column/jdbc-type          data_type
                 :attr.column/db-type            type_name
-                :attr.column/ident              (column-ident db-config column-meta-data)})
+                :attr.column/ident              (column-ident db-config column-meta-data)
+                :attr.column/ordinal-position   ordinal_position})
      [:entities entity-ident entity-attr-qualifier]
      conj attr-ident)))
 
@@ -87,13 +88,30 @@
        (filter-columns db-config)
        (reduce (partial add-attribute-meta-data db-config) heql-meta-data)))
 
+(defn- add-primary-keys-meta-data [db-spec jdbc-meta-data db-config heql-meta-data]
+  (let [schemas-to-ignore (get-in db-config [:schema :ignore])]
+    (->> (.getPrimaryKeys jdbc-meta-data nil "" nil)
+         (meta-data-result db-spec)
+         (remove #(contains? schemas-to-ignore (:table_schem %)))
+         (group-by (fn [{:keys [table_schem table_name]}]
+                     [table_schem table_name]))
+         (map (fn [[[table_schem table_name] v]]
+            [(entity-ident db-config {:table_schem table_schem
+                                      :table_name  table_name})
+             {:entity.relation/primary-key {:primary-key/name  (:pk_name (first v))
+                                            :primary-key/attrs (set (map #(attribute-ident db-config %) v))}}]))
+         (into {})
+         (merge-with merge (:entities heql-meta-data))
+         (assoc heql-meta-data :entities))))
+
 (defmulti get-db-config identity)
 
 (defn fetch [db-spec]
   (with-open [conn (jdbc/get-connection db-spec)]
-    (let [jdbc-meta-data     (.getMetaData conn)
-          db-product-name    (.getDatabaseProductName jdbc-meta-data)
-          db-config          (assoc (get-db-config db-product-name) :db-product-name db-product-name)
-          entities-meta-data (entities-meta-data db-spec jdbc-meta-data db-config)
-          heql-meta-data     {:entities entities-meta-data}]
-      (add-attributes-meta-data db-spec jdbc-meta-data db-config heql-meta-data))))
+    (let [jdbc-meta-data  (.getMetaData conn)
+          db-product-name (.getDatabaseProductName jdbc-meta-data)
+          db-config       (assoc (get-db-config db-product-name) :db-product-name db-product-name)]
+      (->> (entities-meta-data db-spec jdbc-meta-data db-config)
+           (hash-map :entities)
+           (add-attributes-meta-data db-spec jdbc-meta-data db-config)
+           (add-primary-keys-meta-data db-spec jdbc-meta-data db-config)))))
